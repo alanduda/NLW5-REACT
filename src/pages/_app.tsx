d8e2a2e585330551867330a37645eef9013aad81
Alan Duda <alandudatop10@gmail.com>
2021-06-24 19:30:44 -0300

    Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.scss'; 
 
 import { Header } from './components/Header';
+import { Player } from './components/Player';
 
 import styles from '../styles/app.module.scss'
 
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <Header />
         <Component {...pageProps} /> 
       </main>
+      <Player></Player>
     </div>
   );
 }
