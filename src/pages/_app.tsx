4e32dc7f5bf40840bf522b61067ca9313a9dfc47
Alan Duda <alandudatop10@gmail.com>
2021-06-24 15:13:53 -0300

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/global.scss'; 
 
+import { Header } from './components/Header';
+
+import styles from '../styles/app.module.scss'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} /> 
+      </main>
+    </div>
+  );
 }
 
 export default MyApp
